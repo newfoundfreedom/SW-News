ef24a26737e1b4a075d655865ce1a3a9ed35db3c
Joel Roberts <newfoundfreedom@gmail.com>
2017-05-18 10:15:41 -0400

    handlebars added with .hbs extension

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // Package dependency declarations
-const express = require("express");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const express = require('express');
+const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
 
 // App initialization
@@ -13,7 +14,7 @@ const app = express();
 // Configure morgan and body parser
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 
 // Make public dir static
@@ -21,6 +22,11 @@ app.use(express.static("./public"));
 
 // Routes
 require("./routes/api-routes.js")(app);
+
+// configure handlebars
+const exphbs = require("express-handlebars");
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: '.hbs' }));
+app.set("view engine", "hbs");
 
 
 
@@ -33,17 +39,17 @@ const db = mongoose.connection;
 
 // Show any mongoose errors
 db.on("error", function(error) {
-  console.log("Mongoose Error: ", error);
+    console.log("Mongoose Error: ", error);
 });
 
 // Successful db login message
 db.once("open", function() {
-  console.log("Successful db connection.");
+    console.log("Successful db connection.");
 });
 
 
 
 // Start server listening
 app.listen(PORT, function() {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
